c41b7b837a4e4fd791faad63870ecc1891722000
Qaisar Abbas <161966447+QaisarAbbas2024@users.noreply.github.com>
2025-03-09 16:40:55 +0500

    Create Day-04.py

---
+++ Day-04.py
@@ -0,0 +1,16 @@
+#### conditions.py
+```python
+age = int(input("Enter your age: "))
+if age >= 18:
+    print("You are an adult.")
+elif age > 12:
+    print("You are a teenager.")
+else:
+    print("You are a child.")
+```
+
+#### exercises.py
+```python
+# 1. Ask the user for a number and check if it is even or odd.
+# 2. Write a program to check if a number is positive, negative, or zero.
+```
