4bc60c8f3fe4a55ca5dc78e29fb38882419f8836
Qaisar Abbas <161966447+QaisarAbbas2024@users.noreply.github.com>
2025-03-09 16:36:53 +0500

    Create Day-03.py

---
+++ Day-03.py
@@ -0,0 +1,13 @@
+#### operators.py
+```python
+x = 10
+y = 5
+print("Sum:", x + y)
+print("Is x greater than y?", x > y)
+```
+
+#### exercises.py
+```python
+# 1. Write a program to calculate the area of a rectangle.
+# 2. Ask the user for two numbers and perform basic arithmetic operations.
+```
